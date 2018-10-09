d748a6b2af8bf43372564dd95afeff7fb2546e21
RobertoSRocha <roberto_rocha_11@hotmail.com>
2018-10-09 09:39:38 -0300

    Ajuste na Header do operacional

--- inc/header_operacional.php
+++ inc/header_operacional.php
@@ -45,7 +45,7 @@
 
     <header class="main-header">
         <!-- Logo -->
-        <a href="<?php echo BASEURL; ?>index.php" class="logo">
+        <a href="<?php echo BASEURL; ?>operacional/index.php" class="logo">
             <!-- mini logo for sidebar mini 50x50 pixels -->
             <span class="logo-mini"><b>SAA</b></span>
             <!-- logo for regular state and mobile devices -->
