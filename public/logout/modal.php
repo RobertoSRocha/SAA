b9b11ac9456fac68bd36b496a437346684f6baf6
RobertoSRocha <roberto_rocha_11@hotmail.com>
2018-11-07 12:47:21 -0300

    Tela splash de logon

--- public/logout/modal.php
+++ public/logout/modal.php
@@ -1,28 +1,11 @@
 <?php
     require_once '../../config.php';
 ?>
-
-<!DOCTYPE html>
-<html lang="pt-br">
-<head>
-    <meta charset="utf-8">
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SAA-EMCM</title>
- <style>
-    .load {
-    width: 100px;
-    height: 100px;
-    position: absolute;
-    top: 30%;
-    left: 45%;
-    color: blue;
- }
-</style>
-</head>
-<body>
-    <div class="load"> <img src="<?php echo BASEURL; ?>dist/img/loadin.gif"></div>
-</body>
-</html>
-
-<?php
-    require_once 'sleep.php';
+<?php include(HEADER_TEMPLATE_LOGOUT); ?>
+<!-- Main conteudoCentral -->
+    <section class="content">
+        <img src="<?php echo BASEURL; ?>dist/img/loading.gif" class="img-responsive center-block " alt="loading">
+        <img src="<?php echo BASEURL; ?>dist/img/3i.gif" class="img-responsive center-block " alt="loading">
+    </section>
+<!-- /.content -->
+<?php include(FOOTER_TEMPLATE); ?>
