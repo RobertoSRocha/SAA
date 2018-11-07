1eca41ce69f726205506b8dab248e9bd62e20e73
RobertoSRocha <roberto_rocha_11@hotmail.com>
2018-11-07 18:04:06 -0300

    Telas da operacional organizadas

--- operacional/setor/index.php
+++ operacional/setor/index.php
@@ -32,7 +32,6 @@
             </ol>		
         </div>			
         <div class="breadcrumb text-right">		    	
-            <button class="btn btn-primary" disabled=""><i class="fa fa-plus"></i> Novo Setor</button>		    	
             <a class="btn btn-default" href="index.php"><i class="fa fa-refresh"></i> Atualizar</a>		    
         </div>		
     </div>	
@@ -89,9 +88,7 @@
                                         <td>Local não encontrado</td>		
                                     <?php endif; ?>
                                     <td class="actions text-center">
-                                        <a href="view.php?id=<?php echo $setor['id']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>				
-                                        <button class="btn btn-sm btn-warning" disabled=""><i class="fa fa-pencil"></i> Editar</button>				
-                                        <button class="btn btn-sm btn-danger" disabled=""><i class="fa fa-trash"></i> Excluir</button>			
+                                        <a href="view.php?id=<?php echo $setor['id']; ?>" class="btn btn-sm btn-success"><i class="fa fa-eye"></i> Visualizar</a>			
                                     </td>		
                                 </tr>	
                                 <?php endforeach; ?>	
--- public/logout/sleep.php
+++
@@ -1,7 +0,0 @@
-<?php
-    redirecionar();
-    function redirecionar(){
-        sleep(5);
-        header("Location: ".BASEURL."public/index.php");
-    }
-    
