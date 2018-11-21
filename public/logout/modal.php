f1b857242522a39322881db614b82793d26806ae
RobertoSRocha <roberto_rocha_11@hotmail.com>
2018-11-21 14:52:23 -0300

    Versão de testes

--- public/logout/modal.php
+++ public/logout/modal.php
@@ -5,7 +5,6 @@
 <!-- Main conteudoCentral -->
     <section class="content">
         <img src="<?php echo BASEURL; ?>dist/img/loading.gif" class="img-responsive center-block " alt="loading">
-        <img src="<?php echo BASEURL; ?>dist/img/3i.gif" class="img-responsive center-block " alt="loading">
     </section>
 <!-- /.content -->
 <?php include(FOOTER_TEMPLATE); ?>
