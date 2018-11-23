a1957bffcc38290a3bad41ba81698d87193ec27b
ArturCarlos <arturcarlossantiago@gmail.com>
2018-11-23 08:47:06 -0300

    Atualização do banco

--- sql/Banco SAA.sql
+++ sql/Banco SAA.sql
@@ -8,7 +8,7 @@ CREATE TABLE usuario(
     nome VARCHAR(100)  NOT NULL,
     img VARCHAR(255) DEFAULT NULL,
     matricula VARCHAR(25) UNIQUE NOT NULL,
-    email VARCHAR(100)  NOT NULL,
+    email VARCHAR(100) DEFAULT NULL,
     senha VARCHAR(255)  NOT NULL,
     permissao int  NOT NULL,
     PRIMARY KEY (id)
