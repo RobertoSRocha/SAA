8cd511173b7a45402da5ebb49025bab1a74c690d
gersonSEP <gersonn512@gmail.com>
2018-11-26 21:33:43 -0300

    tratamentos formulários

--- admin/formResidencia/index.php
+++ admin/formResidencia/index.php
@@ -28,70 +28,78 @@
         <div class="row">
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-orange-active">
-                    <div class="inner">
-                        <h3>Form 01</h3>
+                <a href="form01.php" class="small-box-footer" title="Afastamento de Atividades">
+                    <div class="small-box bg-orange-active">
+                        <div class="inner">
+                            <h3>Form 01</h3>
 
-                        <p>Afastamento de Atividades</p>
+                            <p>Afastamento de Atividades</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-android-document"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-android-document"></i>
-                    </div>
-                    <a href="form01.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-purple-active">
-                    <div class="inner">
-                        <h3>Form 02</h3>
+                <a href="form02.php" class="small-box-footer" title="Participação em Evento">
+                    <div class="small-box bg-purple-active">
+                        <div class="inner">
+                            <h3>Form 02</h3>
 
-                        <p>Participação em Evento</p>
+                            <p>Participação em Evento</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-android-calendar"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-android-calendar"></i>
-                    </div>
-                    <a href="form02.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-aqua-active">
-                    <div class="inner">
-                        <h3>Form 03</h3>
+                <a href="form03.php" class="small-box-footer" title="Troca de Plantão">
+                    <div class="small-box bg-aqua-active">
+                        <div class="inner">
+                            <h3>Form 03</h3>
 
-                        <p>Troca de Plantão</p>
+                            <p>Troca de Plantão</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion-arrow-swap"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion-arrow-swap"></i>
-                    </div>
-                    <a href="form03.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-green-active">
-                    <div class="inner">
-                        <h3>Form 04</h3>
+                <a target="_blank" href="<?php echo BASEURL; ?>dist/pdf/Formulario_Solicitacao_Estagio_Opcional.pdf" class="small-box-footer">
+                    <div class="small-box bg-green-active">
+                        <div class="inner">
+                            <h3>Form 04</h3>
 
-                        <p>Solicitação de Estágio</p>
+                            <p>Solicitação de Estágio</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-medkit"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-medkit"></i>
-                    </div>
-                    <a target="_blank" href="<?php echo BASEURL; ?>dist/pdf/Formulario_Solicitacao_Estagio_Opcional.pdf" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
         </div>
--- public/formGraduacao/index.php
+++ public/formGraduacao/index.php
@@ -24,70 +24,78 @@
         <div class="row">
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-aqua">
-                    <div class="inner">
-                        <h3>Form 01</h3>
+                <a href="form01.php" class="small-box-footer" title="Requerimentos Gerais">
+                    <div class="small-box bg-aqua">
+                        <div class="inner">
+                            <h3>Form 01</h3>
 
-                        <p>Requerimentos Gerais</p>
+                            <p>Requerimentos Gerais</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-android-document"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-android-document"></i>
-                    </div>
-                    <a href="form01.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-green">
-                    <div class="inner">
-                        <h3>Form 02</h3>
+                <a href="form02.php" class="small-box-footer" title="Auxílio Financeiro">
+                    <div class="small-box bg-green">
+                        <div class="inner">
+                            <h3>Form 02</h3>
 
-                        <p>Auxílio Financeiro</p>
+                            <p>Auxílio Financeiro</p>
+                        </div>
+                        <div class="icon">
+                            <i class="fa fa-money"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="fa fa-money"></i>
-                    </div>
-                    <a href="form02.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-yellow">
-                    <div class="inner">
-                        <h3>Form 03</h3>
+                <a href="form03.php" class="small-box-footer" title="Exercícios Domiciliares - Evento">
+                    <div class="small-box bg-yellow">
+                        <div class="inner">
+                            <h3>Form 03</h3>
 
-                        <p>Exercícios Domiciliares - Evento</p>
+                            <p>Exercícios Domiciliares - Evento</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-document"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-document"></i>
-                    </div>
-                    <a href="form03.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
             <div class="col-lg-3 col-xs-6">
                 <!-- small box -->
-                <div class="small-box bg-red">
-                    <div class="inner">
-                        <h3>Form 04</h3>
+                <a href="form04.php" title="Exercícios Domiciliares - Saúde" class="small-box-footer">
+                    <div class="small-box bg-red">
+                        <div class="inner">
+                            <h3>Form 04</h3>
 
-                        <p>Exercícios Domiciliares - Saúde</p>
+                            <p>Exercícios Domiciliares - Saúde</p>
+                        </div>
+                        <div class="icon">
+                            <i class="ion ion-document-text"></i>
+                        </div>
+
+                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+
                     </div>
-                    <div class="icon">
-                        <i class="ion ion-document-text"></i>
-                    </div>
-                    <a href="form04.php" class="small-box-footer">
-                        Requisitar <i class="fa fa-arrow-circle-right"></i>
-                    </a>
-                </div>
+                </a>
             </div>
             <!-- ./col -->
         </div>
