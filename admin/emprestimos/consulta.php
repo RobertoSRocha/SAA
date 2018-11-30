3e9745ba15f1c07776a4b548528716e004eb73cd
ArturCarlos <arturcarlossantiago@gmail.com>
2018-11-30 12:03:36 -0300

    Filtros consultar emprestimos

--- admin/emprestimos/consulta.php
+++ admin/emprestimos/consulta.php
@@ -2,10 +2,13 @@
 <?php require_once DBAPI; ?>
 
 <?php
-    require_once LOGIN2;
-    verificaLoginAdmin();
+require_once LOGIN2;
+verificaLoginAdmin();
 ?>
-
+<?php
+require_once PATRIMONIO;
+indexPatrimonio();
+?>
 <?php
 require_once EMPRESTIMOS;
 filtro();
@@ -86,6 +89,56 @@ filtro();
                     </form>
                 </div>
 
+
+                <div class="box text-center">
+                    <hr/>
+                    <h3>Listagem de usuários do sistema</h3>
+                    <hr/>
+                </div>
+                <div class="box-body">
+
+                    <table id="tab_usuario" class="table table-bordered table-hover">
+                        <thead>
+                        <tr>
+
+                            <th title="Ordenar Tabela">Nome</th>
+                            <th title="Ordenar Tabela">Tombo</th>
+                            <th title="Ordenar Tabela">Especificação</th>
+                            <th title="Ordenar Tabela">Setor</th>
+                            <th title="Ordenar Tabela">Local</th>
+                            <th>Ações</th>
+                        </tr>
+                        </thead>
+                        <tbody>
+                        <?php if ($patrimonios) : ?>
+                            <?php foreach ($patrimonios as $patrimonio) : ?>
+                                <tr>
+                                    <td><?php echo $patrimonio['nome']; ?></td>
+                                    <td><?php echo $patrimonio['tombo']; ?></td>
+                                    <td><?php echo $patrimonio['especificacao']; ?></td>
+                                    <td><?php echo $patrimonio['setor_id']; ?></td>
+
+                                </tr>
+                            <?php endforeach; ?>
+                        <?php else : ?>
+                            <tr>
+                                <td colspan="6">Nenhum registro encontrado.</td>
+                            </tr>
+                        <?php endif; ?>
+                        </tbody>
+
+                        <tfoot>
+                        <tr style="background: #F4F4F4">
+                            <th>FILTROS</th>
+                            <th></th>
+                            <th></th>
+                            <th title="Filtrar por categoria"></th>
+                            <th title="Filtrar por permissão"></th>
+                            <th></th>
+                        </tr>
+                        </tfoot>
+                    </table>
+                </div>
             </div>
         </div>
     </div>
