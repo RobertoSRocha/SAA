928f40dae5c3fa7caf0fa2743bfbed6d57e147e8
Eliabe Felix <eliabemf@gmail.com>
2020-03-02 16:33:24 -0300

    formas de visualizar chamados

--- admin/chamado/index.php
+++ admin/chamado/index.php
@@ -43,106 +43,149 @@
     <div class="col-xs-12">
         <div class="row">
 
-            <div class="col-md-4">
-                <!-- Widget: user widget style 1 -->
-                <div class="box box-widget widget-user">
-                    <!-- Add the bg color to the header using any of the bg-* classes -->
-                    <div class="widget-user-header bg-yellow">               
-                        <!-- /.widget-user-image -->
-                        <h3 class="widget-user-username">Nadia Carmichael</h3>
-                        <h5 class="widget-user-desc">Lead Developer</h5>
-                    </div>
-                    <div class="box-footer no-padding">
-                   <p>
-                       sdfasdfasdf
-                       asdfasdfasdfasdfas ewrwe rwe rfjsdkfjasd fjasdj adasd
-                       fasdf
-                       asdfasdfasdfasdfas
-                       dfasdfasdfasdf
 
-                   </p>
+        <style>            
+            .chamado-box-head-yellow{
+                background-color: #f39c12;
+                padding-top: 2%;
+                padding-bottom: 2%;
+                padding-left: 3%;
+            }
+            .chamado-box-head-red{
+                background-color: #dd4b39;
+                padding-top: 2%;
+                padding-bottom: 2%;
+                padding-left: 3%;
+            }
+            .chamado-box-head-blue{
+                background-color: #00c0ef ;
+                padding-top: 2%;
+                padding-bottom: 2%;
+                padding-left: 3%;
+            }
+           
+            .chamado-box-user{
+                margin: 0 0 0 0;
+                font-size: 25px;
+                color: #ffffff;
+            }
+            .chamado-box-setor{ 
+                margin: 0 0 0 0;
+                font-size: 17px;
+                color: #ffffff;
+            }
+
+            .chamado-box-body{
+                background-color: #ffffff;
+                color: #000000;
+                padding-top: 2%;
+                padding-bottom: 2%;
+                padding-left: 3%;
+                padding-right: 3%;
+            }
+
+
+        </style>
+
+            <div class="col-md-3">
+            
+                <div class="chamado-box">
+
+                    <div class="chamado-box-head-blue">                                                
+                        <p class="chamado-box-user">Nadia Carmichael</p>
+                        <p class="chamado-box-setor">Nadia Carmichael</p>
+                    </div>
+                
+                    <div class="chamado-box-body">
+                        <p>
+                        sdfasdfasdf
+                        asdfasdfasdfasdfas ewrwe rwe rfjsdkfjasd fjasdj adasd
+                        fasdf
+                        asdfasdfasdfasdfas
+                        dfasdfasdfasdf asdf asd ftp_allocdf addslashesfsad ftp_allocasdf 
+                        asdfa spl_autoload_functionsasdfasdf
+                        asdfasdfasdfasdfasasd
+                        asdfasdfasdfasdfas
+
+                        </p>
                     </div>
                 </div>
-                <!-- /.widget-user -->
             </div>
 
+            <div class="col-md-3">
+        
+                <div class="chamado-box">
 
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <a href="form01.php" class="small-box-footer" title="Requerimentos Gerais">
-                    <div class="small-box bg-aqua">
-                        <div class="inner">
-                            <h3>Geral</h3>
-
-                            <p>Requerimentos Gerais</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-android-document"></i>
-                        </div>
-
-                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="chamado-box-head-yellow">                                                
+                        <p class="chamado-box-user">Nadia Carmichael</p>
+                        <p class="chamado-box-setor">Nadia Carmichael</p>
 
                     </div>
-                </a>
+                    <div class="chamado-box-body">
+                        <p>
+                            sdfasdfasdf
+                            asdfasdfasdfasdfas ewrwe rwe rfjsdkfjasd fjasdj adasd
+                            fasdf
+                            asdfasdfasdfasdfas
+                            dfasdfasdfasdf asdf asd ftp_allocdf addslashesfsad ftp_allocasdf 
+                            asdfa spl_autoload_functionsasdfasdf
+                            asdfasdfasdfasdfasasd
+                            asdfasdfasdfasdfas
+
+                        </p>
+                    </div>
+                </div>
+                
             </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <a href="form02.php" class="small-box-footer" title="Auxílio Financeiro">
-                    <div class="small-box bg-green">
-                        <div class="inner">
-                            <h3>Financeiro</h3>
 
-                            <p>Auxílio Financeiro</p>
-                        </div>
-                        <div class="icon">
-                            <i class="fa fa-money"></i>
-                        </div>
+            <div class="col-md-3">
+        
+                <div class="chamado-box">
 
-                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="chamado-box-head-red">                                                
+                        <p class="chamado-box-user">Nadia Carmichael</p>
+                        <p class="chamado-box-setor">Nadia Carmichael</p>
 
                     </div>
-                </a>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <a href="form03.php" class="small-box-footer" title="Exercícios Domiciliares - Evento">
-                    <div class="small-box bg-yellow">
-                        <div class="inner">
-                            <h3>Evento</h3>
+                    <div class="chamado-box-body">
+                        <p>
+                            sdfasdfasdf
+                            asdfasdfasdfasdfas ewrwe rwe rfjsdkfjasd fjasdj adasd
+                            fasdf
+                            asdfasdfasdfasdfas
+                            dfasdfasdfasdf asdf asd ftp_allocdf addslashesfsad ftp_allocasdf 
+                            asdfa spl_autoload_functionsasdfasdf
+                            asdfasdfasdfasdfasasd
+                            asdfasdfasdfasdfas
 
-                            <p>Exercícios Domiciliares - Evento</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-document"></i>
-                        </div>
+                        </p>
+                    </div>
+                </div>            
+            </div> 
+            <div class="col-md-3">
+        
+                <div class="chamado-box">
 
-                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
+                    <div class="chamado-box-head-red">                                                
+                        <p class="chamado-box-user">Nadia Carmichael</p>
+                        <p class="chamado-box-setor">Nadia Carmichael</p>
 
                     </div>
-                </a>
-            </div>
-            <!-- ./col -->
-            <div class="col-lg-3 col-xs-6">
-                <!-- small box -->
-                <a href="form04.php" title="Exercícios Domiciliares - Saúde" class="small-box-footer">
-                    <div class="small-box bg-red">
-                        <div class="inner">
-                            <h3>Saúde</h3>
+                    <div class="chamado-box-body">
+                        <p>
+                            sdfasdfasdf
+                            asdfasdfasdfasdfas ewrwe rwe rfjsdkfjasd fjasdj adasd
+                            fasdf
+                            asdfasdfasdfasdfas
+                            dfasdfasdfasdf asdf asd ftp_allocdf addslashesfsad ftp_allocasdf 
+                            asdfa spl_autoload_functionsasdfasdf
+                            asdfasdfasdfasdfasasd
+                            asdfasdfasdfasdfas
 
-                            <p>Exercícios Domiciliares - Saúde</p>
-                        </div>
-                        <div class="icon">
-                            <i class="ion ion-document-text"></i>
-                        </div>
-
-                        <span class="small-box-footer">Requisitar<i class="fa fa-arrow-circle-right"></i></span>
-
+                        </p>
                     </div>
-                </a>
+                </div>            
             </div>
-            <!-- ./col -->
         </div>
     </div>
 </section>
